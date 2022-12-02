abd23ecde6d66957622b1955121f38ddc53b6f0b
Ryan McDougall <sempuki1@gmail.com>
2022-12-02 15:44:23 -0800

    reduce some boilerplate

--- hello.cpp
+++ hello.cpp
@@ -1,24 +1,17 @@
 #include <SDL.h>
-#include <stdio.h>
 
+#include <cstdio>
 #include <iostream>
 
 #include "backends/imgui_impl_sdl.h"
 #include "backends/imgui_impl_sdlrenderer.h"
 #include "imgui.h"
 
-#if !SDL_VERSION_ATLEAST(2, 0, 17)
-#error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
-#endif
-
 // Main code
 int main(int, char**) {
   // Setup SDL
-  // (Some versions of SDL before <2.0.10 appears to have performance/stalling issues on a minority
-  // of Windows systems, depending on whether SDL_INIT_GAMECONTROLLER is enabled or disabled..
-  // updating to the latest version of SDL is recommended!)
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
-    printf("Error: %s\n", SDL_GetError());
+    std::printf("Error: %s\n", SDL_GetError());
     return -1;
   }
 
@@ -38,9 +31,6 @@ int main(int, char**) {
     SDL_Log("Error creating SDL_Renderer!");
     return 0;
   }
-  // SDL_RendererInfo info;
-  // SDL_GetRendererInfo(renderer, &info);
-  // SDL_Log("Current SDL_Renderer: %s", info.name);
 
   // Setup Dear ImGui context
   IMGUI_CHECKVERSION();
@@ -52,33 +42,10 @@ int main(int, char**) {
 
   // Setup Dear ImGui style
   ImGui::StyleColorsDark();
-  // ImGui::StyleColorsLight();
 
   // Setup Platform/Renderer backends
   ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
   ImGui_ImplSDLRenderer_Init(renderer);
-
-  // Load Fonts
-  // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple
-  // fonts and use ImGui::PushFont()/PopFont() to select them.
-  // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the
-  // font among multiple.
-  // - If the file cannot be loaded, the function will return NULL. Please handle those errors in
-  // your application (e.g. use an assertion, or display an error and quit).
-  // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when
-  // calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-  // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality
-  // font rendering.
-  // - Read 'docs/FONTS.md' for more instructions and details.
-  // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to
-  // write a double backslash \\ !
-  // io.Fonts->AddFontDefault();
-  // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-  // ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL,
-  // io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != NULL);
 
   // Our state
   bool show_demo_window = true;
@@ -88,14 +55,6 @@ int main(int, char**) {
   // Main loop
   bool done = false;
   while (!done) {
-    // Poll and handle events (inputs, window resize, etc.)
-    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui
-    // wants to use your inputs.
-    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main
-    // application, or clear/overwrite your copy of the mouse data.
-    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main
-    // application, or clear/overwrite your copy of the keyboard data. Generally you may always pass
-    // all inputs to dear imgui, and hide them from your application based on those two flags.
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
       ImGui_ImplSDL2_ProcessEvent(&event);
@@ -110,11 +69,7 @@ int main(int, char**) {
     ImGui_ImplSDL2_NewFrame();
     ImGui::NewFrame();
 
-    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can
-    // browse its code to learn more about Dear ImGui!).
-    if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
-
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named
+    // Show a simple window that we create ourselves. We use a Begin/End pair to create a named
     // window.
     {
       static float f = 0.0f;
@@ -140,17 +95,6 @@ int main(int, char**) {
       ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
                   1000.0f / ImGui::GetIO().Framerate,
                   ImGui::GetIO().Framerate);
-      ImGui::End();
-    }
-
-    // 3. Show another simple window.
-    if (show_another_window) {
-      ImGui::Begin(
-        "Another Window",
-        &show_another_window);  // Pass a pointer to our bool variable (the window will have a
-                                // closing button that will clear the bool when clicked)
-      ImGui::Text("Hello from another window!");
-      if (ImGui::Button("Close Me")) show_another_window = false;
       ImGui::End();
     }
 
